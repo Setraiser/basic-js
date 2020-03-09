05e1594b9427775568f931140fa3b25a8eb25cb1
Karpuk Konstantin <karpukkonstantin@gmail.com>
2020-03-09 21:33:58 +0300

    feat: solution of dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length == 0) return false;
+
+  let result;
+  let arr = [];
+  let regFindSpace = /\s+/gi;
+
+  members.sort().forEach((item) => {
+    if (typeof item === 'string') {
+      item = item.replace(regFindSpace, '').substr(0,1).toUpperCase();
+      arr.push(item);
+    }
+    
+    result = arr.sort().join('');
+    
+  });
+  return result;
 };
