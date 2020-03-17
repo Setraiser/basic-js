2f3a3edb7427b58bfba8bc2ed6d8aeddc86749b7
Karpuk Konstantin <karpukkonstantin@gmail.com>
2020-03-17 14:32:15 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,47 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(direct=true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    orderA(char) {
+        return char.charCodeAt(0) - 65;
+    }
+
+    crypt(string, key, isDirect) {
+        if (string === undefined || key === undefined) throw Error();
+        let str;
+        let newWordArr = [];
+        let counter = 0;
+        let reg = /[A-Z]/g;
+        let b;
+
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+        str = string.replace(reg, (letter) => {
+            b = key[counter++ % key.length];
+            return String.fromCharCode(((this.orderA(letter) + (isDirect ? 26 - this.orderA(b) : this.orderA(b))) % 26 + 65));
+        });
+
+        if (!this.direct) {
+            for (let i = str.length; i >=0; i--) {
+                if (str[i] >= 'A' || str[i] <= 'Z') {
+                    newWordArr.push(str[i]);
+                }
+            }
+
+            str = newWordArr.join('');
+
+        }
+        return str;
+    }
+
+    encrypt(string, key) {
+        return this.crypt(string, key, false);
+    }
+
+    decrypt(string, key) {
+        return this.crypt(string, key, true); 
     }
 }
 
